c001e045e965dc497725903a3e6568e69b77b58d
arthursb2016 <artsborba@gmail.com>
2023-01-18 10:29:52 -0300

    vuetify is on

--- src/main.js
+++ src/main.js
@@ -2,4 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import { VSelect, VIcon, VCheckbox }from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components: { VSelect, VIcon, VCheckbox },
+  directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
