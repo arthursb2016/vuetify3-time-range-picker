a67dca4c3221c0184abfa35e84f7995f3e613e80
arthursb2016 <artsborba@gmail.com>
2023-01-24 10:18:06 -0300

    VApp (just because)

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import App from './App.vue'
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
+import { VApp } from 'vuetify/components'
 
-const vuetify = createVuetify({})
+const vuetify = createVuetify({ components: { VApp } })
 
 createApp(App).use(vuetify).mount('#app')
